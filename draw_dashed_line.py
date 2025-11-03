936ff73fff1e2ef0ac0d463239d36d5e8d5f322d
Ali Qazipoor <qazipoor@gmail.com>
2025-11-03 06:47:28 +0300

    Draw dashed line functionality

---
+++ draw_dashed_line.py
@@ -0,0 +1,23 @@
+import turtle as t
+
+screem = t.Screen()
+
+tim = t.Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+
+
+
+
+
+
+
+
+
+
+screem.exitonclick()
